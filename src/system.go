1246742dca8484209eb822ab3a62dfd5a22525b8
Eiton <micheal257815@gmail.com>
2024-11-30 21:01:53 +0800

    Make sure all loading tasks are finished before start

--- src/system.go
+++ src/system.go
@@ -2324,6 +2324,8 @@ func (s *System) fight() (reload bool) {
 		s.nextRound()
 		s.roundResetFlg, s.introSkipped = false, false
 		s.reloadFlg, s.reloadStageFlg, s.reloadLifebarFlg = false, false, false
+		s.runMainThreadTask()
+		gfx.Await()
 	}
 	reset()
 
